3c23868f38473738b6389c747f0952dc5764270c
Raphael Matori <5611205+raphaelmatori@users.noreply.github.com>
2025-05-28 08:39:16 +0200

    chore: updating pipelines

--- backend/src/main/java/com/f1/app/controller/ChampionController.java
+++ backend/src/main/java/com/f1/app/controller/ChampionController.java
@@ -1,7 +1,17 @@
 package com.f1.app.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.f1.app.dto.ChampionDTO;
 import com.f1.app.service.ChampionService;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -11,10 +21,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Slf4j
 @RestController
--- mysql/init/01-init.sql
+++
@@ -1,7 +0,0 @@
--- Create database if not exists
-CREATE DATABASE IF NOT EXISTS f1_champions;
-USE f1_champions;
-
--- Grant privileges to the application user
-GRANT ALL PRIVILEGES ON f1_champions.* TO 'f1_user'@'%';
-FLUSH PRIVILEGES; 
